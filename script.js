07dae58e3f0b995af52f1ddd38c2bc8e29e46458
AndreasRG <andreas@revesz-gudmann.dk>
2024-10-07 19:06:06 +0200

    Snapshot 1.0 Corrected starting value of highscore = 20 to 0 in HTML
    Added javascript code that generates random number and and compares with input, only when a valid number between 1 and 20 is entered

--- script.js
+++ script.js
@@ -1,1 +1,66 @@
-'use strict';
+//Selecting all individual elements
+const inputValueSelector = document.querySelector(".guess");
+const checkButton = document.querySelector("main button");
+const currentScoreSelector = document.querySelector(".score");
+const currentHighscoreSelector = document.querySelector(".highscore");
+
+
+let valueCurrentScore = 0;
+let valueCurrentHighscore = 0;
+
+
+checkButton.addEventListener("click", () => {
+    //Valid input number check and value of input save
+    let currentValidNumber = 0;
+    let booleanValidNumber = 0;
+        if (inputValueSelector.value >= 1 && inputValueSelector.value <= 20) {
+            currentValidNumber = parseInt(inputValueSelector.value);
+            booleanValidNumber = 1;
+            //Debug
+            console.log(`CurrentValidNumber: ${currentValidNumber}`);
+            console.log(booleanValidNumber);
+        }
+        else {
+            booleanValidNumber = 0;
+            //Debug
+            console.log("False")
+            console.log(booleanValidNumber);
+        }
+    //Random number generator
+    const randomNumber = document.querySelector(".number");
+        let randomNumberValue = -1;
+    if (booleanValidNumber === 1) {
+        randomNumberValue = Math.floor(Math.random() * 20 + 1);
+        randomNumber.textContent = `${randomNumberValue}`;
+    }
+    else {
+        randomNumber.textContent = `?`;
+    }
+    //Comparing random number with input and updating score/highscore
+    if (currentValidNumber === randomNumberValue) {
+        valueCurrentScore = 0;
+        checkHighScore();
+        //Debug
+        console.log("true they are same");
+    }
+    else if (booleanValidNumber === 1){
+        valueCurrentScore++;
+        currentScoreSelector.textContent = valueCurrentScore;
+        inputValueSelector.value = "";
+        //Debug
+        console.log("false they are different");
+        console.log(`This is current score ${currentScoreSelector.textContent}`);
+    }
+})
+
+
+const checkHighScore = () => {
+    if (valueCurrentHighscore == 0 || valueCurrentScore < valueCurrentHighscore) {
+        currentHighscoreSelector.textContent = valueCurrentScore;
+
+    }
+}
+
+
+//Debug:
+console.log(currentHighscoreSelector.textContent);
